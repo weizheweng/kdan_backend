6d09c192bba8c8ee2146534f602b8b5957a26ae4
Zack <zack@kapito.io>
2025-03-15 22:14:41 +0800

    feat: Improve environment variable loading in configuration
    - Simplify loading of the default .env file.
    - Enhance support for environment-specific .env files by overriding existing variables if specified.

--- src/kdan_backend/app/config.py
+++ src/kdan_backend/app/config.py
@@ -2,8 +2,12 @@ import os
 from dotenv import load_dotenv
 
 # 載入對應環境的 .env 文件
-env = os.getenv('ENV', 'dev')
-load_dotenv(f".env.{env}")
+load_dotenv(".env")
+
+# 如果有指定環境變數，則再載入對應的環境設定檔
+env = os.getenv('ENV')
+if env:
+    load_dotenv(f".env.{env}", override=True)
 
 def get_database_url() -> str:
     """獲取資料庫連接字串"""
